962ced946ede87b6bb3b5582a1021c80fc8e503e
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-07-02 00:31:36 +0700

    nyobain alinea

--- cms.tsx
+++ cms.tsx
@@ -1,34 +1,64 @@
-import {Config, Field} from 'alinea'
-import {createCMS} from 'alinea/next'
+import { Config, Field } from "alinea";
+import { createCMS } from "alinea/next";
 
-const Page = Config.type('Page', {
+const Page = Config.type("Page", {
   fields: {
-    title: Field.text('Title'),
-    path: Field.path('Path')
-  }
-})
+    title: Field.text("Title"),
+    path: Field.path("Path"),
+  },
+});
+const WebsiteDetail = Config.type("Website Detail", {
+  fields: {
+    title: Field.text("Judul"),
+    judul_website: Field.text("Judul Website"),
+    deskripsi: Field.text("Deskripsi"),
+    logo: Field.image("Logo"),
+    favicon: Field.image("Favicon"),
+    slogan: Field.text("Slogan"),
+    nomor_wa: Field.text("Nomor WA"),
+    text_chat_wa: Field.text("Text Chat WA"),
+    label_wa: Field.text("Label WA"),
+  },
+});
 
 export const cms = createCMS({
   schema: {
-    Page
+    // Page,
+    WebsiteDetail,
   },
   workspaces: {
-    main: Config.workspace('Example', {
-      source: 'content',
-      mediaDir: 'public',
+    main: Config.workspace("CMS Bimbel Sholuna", {
+      source: "content",
+      mediaDir: "public",
       roots: {
-        pages: Config.root('Example project', {
-          contains: ['Page'],
+        pages: Config.root("Edit Halaman", {
+          contains: ["WebsiteDetail"],
           entries: {
-            welcome: Config.page(
-              Page({
-                title: 'Welcome'
+            // welcome: Config.page(
+            // Page({
+            //   title: "Welcome",
+            // })
+            // ),
+
+            WebsiteDetail: Config.page(
+              WebsiteDetail({
+                title: "Overview",
+                judul_website: "Bimbel Sholuna",
+                deskripsi:
+                  "Bimbel Sholuna adalah bimbingan belajar online yang menyediakan layanan bimbingan belajar online untuk siswa SD, SMP, SMA, dan SMK.",
+                // logo: "/images/logo.png",
+                // favicon: "/images/favicon.png",
+                slogan: "Bimbel Sholuna, Bimbingan Belajar Online Terbaik",
+                nomor_wa: "6281234567890",
+                text_chat_wa:
+                  "Halo, saya ingin bertanya tentang bimbel Sholuna.",
+                label_wa: "Chat WA",
               })
-            )
-          }
+            ),
+          },
         }),
-        media: Config.media()
-      }
-    })
-  }
-})
+        media: Config.media(),
+      },
+    }),
+  },
+});
