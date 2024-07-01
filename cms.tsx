4dae23cfb68d0e783f708fb38221d5987c9d7eca
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-07-01 23:46:37 +0700

    alinea

---
+++ cms.tsx
@@ -0,0 +1,34 @@
+import {Config, Field} from 'alinea'
+import {createCMS} from 'alinea/next'
+
+const Page = Config.type('Page', {
+  fields: {
+    title: Field.text('Title'),
+    path: Field.path('Path')
+  }
+})
+
+export const cms = createCMS({
+  schema: {
+    Page
+  },
+  workspaces: {
+    main: Config.workspace('Example', {
+      source: 'content',
+      mediaDir: 'public',
+      roots: {
+        pages: Config.root('Example project', {
+          contains: ['Page'],
+          entries: {
+            welcome: Config.page(
+              Page({
+                title: 'Welcome'
+              })
+            )
+          }
+        }),
+        media: Config.media()
+      }
+    })
+  }
+})
