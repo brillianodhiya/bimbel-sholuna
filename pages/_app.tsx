bb3f1be879ee2ec6e6bc6418df399965f46bbfbd
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-07-08 11:21:24 +0700

    preline script hide

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 
 import { fontSans, fontMono } from "@/config/fonts";
 import "@/styles/globals.css";
-import PrelineScript from "@/components/PrelineScript";
+// import PrelineScript from "@/components/PrelineScript";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider navigate={router.push}>
       {/* <NextThemesProvider> */}
-      <PrelineScript />
+      {/* <PrelineScript /> */}
       <Component {...pageProps} />
       {/* </NextThemesProvider> */}
     </NextUIProvider>
@@ -25,3 +25,7 @@ export const fonts = {
   sans: fontSans.style.fontFamily,
   mono: fontMono.style.fontFamily,
 };
+
+export function reportWebVitals(metric: { label: string }) {
+  metric.label === "web-vital" && console.log(metric);
+}
