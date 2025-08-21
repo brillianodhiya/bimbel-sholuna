4eb93287da75017b9ea53be4dd0dd2b75434fd71
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2025-08-21 11:50:59 +0700

    add new gallery

--- components/Galeri.tsx
+++ components/Galeri.tsx
@@ -331,6 +331,390 @@ const Galeri = (props: Props) => {
           <button
             className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
             onClick={() => {
+              openZoomedImage(
+                "/Nathan, Kelas Internasional IUP 2025 diterima di ITB, Binus, UWA Australia, dan UNSW Australia.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Nathan, Kelas Internasional IUP 2025 diterima di ITB, Binus, UWA Australia, dan UNSW Australia"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Nathan, Kelas Internasional IUP 2025 diterima di ITB, Binus, UWA Australia, dan UNSW Australia.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Nathan, Kelas Internasional IUP 2025 diterima di ITB, Binus, UWA
+                Australia, dan UNSW Australia
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Pak Agung, ayah Daffa Kelas IUP ITB 2025, diterima di ITB.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Pak Agung, ayah Daffa Kelas IUP ITB 2025, diterima di ITB"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Pak Agung, ayah Daffa Kelas IUP ITB 2025, diterima di ITB.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Pak Agung, ayah Daffa Kelas IUP ITB 2025, diterima di ITB
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Fachri Maula Akhtar, Kelas UTBK 2025. Diterima di Polbatam dan Unair.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Fachri Maula Akhtar, Kelas UTBK 2025. Diterima di Polbatam dan Unair"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Fachri Maula Akhtar, Kelas UTBK 2025. Diterima di Polbatam dan Unair.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Fachri Maula Akhtar, Kelas UTBK 2025. Diterima di Polbatam dan
+                Unair
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage("/Fachri Maula Akhtar, Unair 2025.jpeg");
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Fachri Maula Akhtar, Unair 2025"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Fachri Maula Akhtar, Unair 2025.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Fachri Maula Akhtar, Unair 2025
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Izaz Jati Naila Afkar, Kelas UTBK 2025, diterima Sastra Inggris UM.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Izaz Jati Naila Afkar, Kelas UTBK 2025, diterima Sastra Inggris UM"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Izaz Jati Naila Afkar, Kelas UTBK 2025, diterima Sastra Inggris UM.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Izaz Jati Naila Afkar, Kelas UTBK 2025, diterima Sastra Inggris
+                UM
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Valerie, Kelas IUP 2025, diterima FKG Unpad.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Valerie, Kelas IUP 2025, diterima FKG Unpad"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Valerie, Kelas IUP 2025, diterima FKG Unpad.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Valerie, Kelas IUP 2025, diterima FKG Unpad
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Ival, Kelas UTBK 2025, Sistem Informasi Univeristas Mulawarman.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Ival, Kelas UTBK 2025, Sistem Informasi Univeristas Mulawarman"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Ival, Kelas UTBK 2025, Sistem Informasi Univeristas Mulawarman.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Ival, Kelas UTBK 2025, Sistem Informasi Univeristas Mulawarman
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage("/Helva, Kelas UTBK 2025, STEI-R ITB.jpeg");
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Helva, Kelas UTBK 2025, STEI-R ITB"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Helva, Kelas UTBK 2025, STEI-R ITB.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Helva, Kelas UTBK 2025, STEI-R ITB
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Naila SMA Pradita Dirgantara, Kelas UTBK 2025, Akuntasi UGM.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Naila SMA Pradita Dirgantara, Kelas UTBK 2025, Akuntasi UGM"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Naila SMA Pradita Dirgantara, Kelas UTBK 2025, Akuntasi UGM.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Naila SMA Pradita Dirgantara, Kelas UTBK 2025, Akuntasi UGM
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Hanif, Privat Mulai 11 SMA, Kelas IUP 2025, Teknik Sipil ITS.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Hanif, Privat Mulai 11 SMA, Kelas IUP 2025, Teknik Sipil ITS"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Hanif, Privat Mulai 11 SMA, Kelas IUP 2025, Teknik Sipil ITS.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Hanif, Privat Mulai 11 SMA, Kelas IUP 2025, Teknik Sipil ITS
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Fakhry Dhiyaul Haq Mujahid, 3 tahun Privat, Teknik Kimia ITS 2025.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Fakhry Dhiyaul Haq Mujahid, 3 tahun Privat, Teknik Kimia ITS 2025"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Fakhry Dhiyaul Haq Mujahid, 3 tahun Privat, Teknik Kimia ITS 2025.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Fakhry Dhiyaul Haq Mujahid, 3 tahun Privat, Teknik Kimia ITS
+                2025
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Fadlan, Mulai kelas 11 SMA, Teknik Elektro UB dan Teknik Elektro UI.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Fadlan, Mulai kelas 11 SMA, Teknik Elektro UB dan Teknik Elektro UI"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Fadlan, Mulai kelas 11 SMA, Teknik Elektro UB dan Teknik Elektro UI.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Fadlan, Mulai kelas 11 SMA, Teknik Elektro UB dan Teknik Elektro
+                UI
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage("/Bianca Luna, SMA Al Mahira, FKG Unair.jpeg");
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Bianca Luna, SMA Al Mahira, FKG Unair"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Bianca Luna, SMA Al Mahira, FKG Unair.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Bianca Luna, SMA Al Mahira, FKG Unair
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Heslian, Privat sejak kelas 10 SMA, Kedokteran Universitas Palangkaraya.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Heslian, Privat sejak kelas 10 SMA, Kedokteran Universitas Palangkaraya"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Heslian, Privat sejak kelas 10 SMA, Kedokteran Universitas Palangkaraya.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Heslian, Privat sejak kelas 10 SMA, Kedokteran Universitas
+                Palangkaraya
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage(
+                "/Elvira, Kelas UTBK 2025, Satra Inggris Universitas Diponegoro.jpeg"
+              );
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Elvira, Kelas UTBK 2025, Satra Inggris Universitas Diponegoro"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Elvira, Kelas UTBK 2025, Satra Inggris Universitas Diponegoro.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Elvira, Kelas UTBK 2025, Satra Inggris Universitas Diponegoro
+              </div>
+            </div>
+          </button>
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
+              openZoomedImage("/Bianca Luna, SMA Al Mahira, FKG Unair.jpeg");
+            }}
+          >
+            <div className="">
+              <Image
+                fetchPriority="auto"
+                alt="Bianca Luna, SMA Al Mahira, FKG Unair"
+                className="w-full h-auto object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                height={500}
+                src="/Bianca Luna, SMA Al Mahira, FKG Unair.jpeg"
+                width={500}
+              />
+            </div>
+            <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+              <div className="text-xs font-bold text-white rounded-lg half-black backdrop-saturate-150 p-2 md:text-lg text-center">
+                Bianca Luna, SMA Al Mahira, FKG Unair
+              </div>
+            </div>
+          </button>
+
+          {/* old */}
+          <button
+            className="group relative block rounded-xl overflow-hidden md:w-1/4 sm:w-[40%] w-full"
+            onClick={() => {
               openZoomedImage("/Beberapa Prestasi siswa 1.jpeg");
             }}
           >
